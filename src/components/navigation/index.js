4f0350d693299fc78208710dc0986be4625b3a15
dzenan <dzenankrlic@gmail.com>
2022-03-03 18:10:32 +0100

    Codecademy project

---
+++ src/components/navigation/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { getPetTypes } from '../../api/petfinder';
+import Logo from '../../assets/logo.svg';
+import Search from '../search';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  const [petTypes, setPetTypes] = useState([]);
+
+  useEffect(() => {
+    async function getPetTypesData() {
+      const { types } = await getPetTypes();
+      setPetTypes(types);
+    }
+
+    getPetTypesData();
+  }, []);
+
+  return (
+    <nav>
+      <div className="nav-logo">
+        <NavLink to="/">
+        <img src={Logo} alt="Petlover" />
+        </NavLink>
+        <Search />
+      </div>
+      <ul className="nav-links">
+        <li key="all">
+          {/* This link should have an activeClassName and exact prop */}
+          <NavLink 
+          to="/" className= "nav-link"
+          activeclassName="nav-link-active"
+          >
+            All Pets
+          </NavLink>
+            
+        </li>
+        {petTypes
+          ? petTypes.map((type) => (
+              <li key={type.name}>
+                {/* These links should have an activeClassName prop */}
+                <NavLink 
+                to={`/${type._links.self.href.split('/').pop()}`} 
+                key={type.name} className="nav-link" 
+                activeclassName="nav-link-active"
+                >
+                  {type.name}s
+                </NavLink>
+                
+                {' '}
+              </li>
+            ))
+          : 'Loading...'}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
